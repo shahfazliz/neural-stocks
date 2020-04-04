7d27f2855afce900f1c9f4ebcae801c589bf27a0
Shahfazliz Shahron <shah.shahron@baesystems.com>
2020-04-04 19:52:16 +0800

    Rough idea what it should have

--- src/App.js
+++ src/App.js
@@ -2,25 +2,24 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Object Models
+// 1. Input Layer
+// 2. Hidden Layer
+// 3. Output Layer
+// 4. Neuron
+
+// Business Logic
+// 1 .Calculate all outputs of   all neurons in the hidden layer
+// 2. Calculate all outputs from all neurons in the output layer
+// 3. Calculate the output layer error
+// 4. Update weight between hidden layer to output layer
+// 5. Calculate the hidden layer error
+// 6. Update weight between input layer to hidden layer
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">
+
+  </div>;
 }
 
 export default App;
