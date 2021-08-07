c7cd49d5b854090899139168f486cb3f687b785f
Shahfazliz Shahron <shahfazliz.shahron@trovicor.com>
2021-08-07 17:35:45 +0800

    Move logic to better named functions

--- app.js
+++ app.js
@@ -4,53 +4,6 @@ import { csvToJson } from './util/AdaptorCSV2JSON.js';
 import moment from 'moment';
 
 class App {
-    constructor() {
-        this
-            .readFileToJson({
-                filepath: './Download Data - INDEX_US_S&P US_SPX.csv',
-            })
-            .then((data) => {
-                let trainingData = this.createTrainingData({
-                    appendString: 'SPX',
-                    data: data.sort((a, b) => moment(a.Date, 'MM/DD/YYYY').diff(moment(b.Date, 'MM/DD/YYYY'))),
-                    numberOfElemet: 10,
-                });
-
-                // console.log(trainingData);
-
-                const test = trainingData[trainingData.length - 1];
-
-                // const net = new brain.NeuralNetwork(this._trainingOptions);
-                // net.train(trainingData);
-
-                this
-                    .continueTraining('./trained.txt', trainingData)
-                    .then((net) => {
-                        const json = net.toJSON();
-
-                        fs.writeFile('./trained.txt', JSON.stringify(json), function (error) {
-                            if (error) {
-                                console.log(error);
-                            }
-                        });
-
-                        console.log('result:', net.run(test.input))
-                        console.log('actual:', test.output);
-                    });
-
-                // const json = net.toJSON();
-
-                // fs.writeFile('./trained.txt', JSON.stringify(json), function (error) {
-                //     if (error) {
-                //         console.log(error);
-                //     }
-                // });
-
-                // console.log('result:', net.run(test.input))
-                // console.log('actual:', test.output);
-            });
-    }
-
     _trainingOptions = {
         activation: 'sigmoid',
         binaryThresh: 0.5,
@@ -61,13 +14,12 @@ class App {
         log: true,
     };
 
+    __trainedFilePath = './trained.txt';
+
     /**
      * Read csv file then convert into json.
-     * Returns Promise
      */
-    readFileToJson({
-        filepath = '',
-    }) {
+    readFromCSVFileToJson(filepath) {
         return new Promise((resolve, reject) => {
             fs.readFile(
                 filepath, 
@@ -80,92 +32,116 @@ class App {
         });
     }
 
+    /**
+     * Create training data with output 
+     */
     createTrainingData({
         appendString = 'N/A',
         data = [],
         numberOfElemet = 10,
+        sortDataFunction,
     }) {
-        // Count max length to iterate
-        // minus 1 because we want to start with second element onwards. this is due we need the previous day close to caluclate the difference with today
-        const maxLength = (Math.floor((data.length - 1) / numberOfElemet) - 1) * numberOfElemet + 1;
-
-        let result = [];
-        for (let i = 1; i < maxLength; i++) {
-            let subResult = {};
-            let counter = 1;
-            for (let j = i; j < i + numberOfElemet; j++) {
-                Object
-                    .keys(data[j])
-                    .forEach(function(key) {
-                        // Copy all values
-                        // subResult[`${appendString}_${key}_${counter}`] = key === 'Date'
-                        //     ? moment(data[j][key], 'MM/DD/YYYY').unix()
-                        //     : parseFloat(data[j][key].replace(/,/, ''));
-                        
-                        // Copy only the prices
-                        // if (key !== 'Date') {
-                        //     subResult[`${appendString}_${key}_${counter}`] = parseFloat(data[j][key].replace(/,/, ''));
-                        // }
-
-                        const yesterdayClose = parseFloat(data[j - 1]['Close'].replace(/,/, ''));
-                        const todayOpen = parseFloat(data[j]['Open'].replace(/,/, ''));
-                        const todayClose = parseFloat(data[j]['Close'].replace(/,/, ''));
-                        const todayHigh = parseFloat(data[j]['High'].replace(/,/, ''));
-                        const todayLow = parseFloat(data[j]['Low'].replace(/,/, ''));
-
-                        // For debugging, see the dates
-                        // subResult[`${appendString}_Date_${counter}`] = data[j]['Date'];
-
-                        // Calculate difference with today
-                        subResult[`${appendString}_Open_${counter}`] = todayOpen - yesterdayClose;
-                        subResult[`${appendString}_Close_${counter}`] = todayClose - yesterdayClose;
-
-                        subResult[`${appendString}_High_${counter}`] = todayOpen <= todayClose 
-                            ? todayHigh - todayClose
-                            : todayHigh - todayOpen;
-                        subResult[`${appendString}_Low_${counter}`] = todayOpen <= todayClose
-                            ? todayLow - todayOpen
-                            : todayLow - todayClose;
-                    });
-                counter += 1;
+        return new Promise((resolve, reject) => {
+            if (data.length - 2 < numberOfElemet) {
+                return reject(`number of element is less than ${numberOfElemet + 2}`);
             }
 
-            // To long or to short 
-            const value = parseFloat(data[i + numberOfElemet]
-                    .Close
-                    .replace(/,/, '')) 
-                - parseFloat(data[i + numberOfElemet - 1]
-                    .Close
-                    .replace(/,/, ''));
+            data = sortDataFunction !== null 
+                && typeof(sortDataFunction) === 'function'
+                ? data.sort(sortDataFunction)
+                : data
 
-            // Push the input and output objects for training
-            result.push({ 
-                input: subResult,
-                output: {
-                    [`${appendString}_Long`]: value >= 0 ? 1 : 0,
-                    [`${appendString}_Short`]: value <= 0 ? 1 : 0,
-                },
-            });
-        }
-        return result;
-    }
+            const startIndex = 1;
+            const maxIndexToIterate = data.length - numberOfElemet - 1;
 
-    continueTraining(filepath, trainingData) {
-        return new Promise((resolve, reject) => {
-            this
-                .loadBrain(filepath)
-                .then((net) => {
-                    net.train(trainingData, this._trainingOptions);
-                    return resolve(net);
+            let result = [];
+
+            // For each data, iterate until maxIndexToIterate
+            for (let i = startIndex; i <= maxIndexToIterate; i++) {
+                let subResult = {};
+                let replaceDateWithCount = 1;
+                
+                // Group data from i to numberOfElements into an object
+                for (let j = i; j < i + numberOfElemet; j++) {
+                    Object
+                        .keys(data[j]) // ['Open', 'Close', 'High', 'Low']
+                        .forEach(key => {
+                            
+                            const yesterdayClose = parseFloat(data[j - 1]['Close'].replace(/,/, ''));
+                            const todayOpen = parseFloat(data[j]['Open'].replace(/,/, ''));
+                            const todayClose = parseFloat(data[j]['Close'].replace(/,/, ''));
+                            const todayHigh = parseFloat(data[j]['High'].replace(/,/, ''));
+                            const todayLow = parseFloat(data[j]['Low'].replace(/,/, ''));
+
+                            // For debugging, see the dates
+                            // subResult[`${appendString}_Date_${replaceDateWithCount}`] = data[j]['Date'];
+
+                            // Calculate difference with today
+                            subResult[`${appendString}_Open_${replaceDateWithCount}`] = todayOpen - yesterdayClose;
+                            subResult[`${appendString}_Close_${replaceDateWithCount}`] = todayClose - yesterdayClose;
+                            subResult[`${appendString}_High_${replaceDateWithCount}`] = todayOpen <= todayClose 
+                                ? todayHigh - todayClose
+                                : todayHigh - todayOpen;
+                            subResult[`${appendString}_Low_${replaceDateWithCount}`] = todayOpen <= todayClose
+                                ? todayLow - todayOpen
+                                : todayLow - todayClose;
+                        });
+                    replaceDateWithCount += 1;
+                }
+
+                // To long or to short 
+                const outputValue = parseFloat(data[i + numberOfElemet]
+                        .Close
+                        .replace(/,/, '')) 
+                    - parseFloat(data[i + numberOfElemet - 1]
+                        .Close
+                        .replace(/,/, ''));
+
+                // Push the input and output objects for training
+                result.push({ 
+                    input: subResult,
+                    output: {
+                        [`${appendString}_Long`]: outputValue >= 0 ? 1 : 0,
+                        [`${appendString}_Short`]: outputValue <= 0 ? 1 : 0,
+                    },
                 });
+            }
+
+            return resolve(result);
         });
     }
 
-    loadBrain(filepath) {
+    startTraining(trainingData) {
         return new Promise((resolve, reject) => {
-            // Use the trained file
+            const net = new brain.NeuralNetwork(this._trainingOptions);
+            net.train(trainingData);
+
+            return this
+                .saveTraining(net)
+                .then(net => resolve(net))
+                .catch(error => reject(error));
+        });
+    }
+
+    saveTraining(net) {
+        return new Promise((resolve, reject) => {
+            fs.writeFile(
+                this.__trainedFilePath, 
+                JSON.stringify(net.toJSON()), 
+                error => {
+                    if (error) {
+                        return reject(error);
+                    }
+                });
+            
+            return resolve(net);
+        });
+    }
+
+    loadTrainedData() {
+        return new Promise((resolve, reject) => {
             fs.readFile(
-                filepath, 
+                this.__trainedFilePath, 
                 'utf8', 
                 (error, data) => {
                     if (error) {
@@ -174,13 +150,52 @@ class App {
                     
                     const net = new brain.NeuralNetwork();
                     net.fromJSON(JSON.parse(data));
-                    // console.log('result:', net.run(test.input))
-                    // console.log('actual:', test.output);
-
                     return resolve(net);
                 });
         });
     }
+
+    continueTraining(trainingData) {
+        return this
+            .loadTrainedData()
+            .then(net => {
+                net.train(trainingData, this._trainingOptions);
+
+                return this.saveTraining(net);
+            });
+    }
 }
 
 const app = new App();
+
+app
+    .readFromCSVFileToJson('./Download Data - INDEX_US_S&P US_SPX.csv') // from https://www.marketwatch.com/investing/index/spx/download-data
+    .then((jsonData) => {
+        return app.createTrainingData({
+            appendString: 'SPX',
+            data: jsonData,
+            numberOfElemet: 10,
+            sortDataFunction: (a, b) => moment(a.Date, 'MM/DD/YYYY').diff(moment(b.Date, 'MM/DD/YYYY')),
+        });
+    })
+    // Create new training
+    .then(trainingData => {
+        return app
+            .startTraining(trainingData)
+            .then(net => {
+                const lastTrainingData = trainingData[trainingData.length - 1];
+                console.log('result:', net.run(lastTrainingData.input))
+                console.log('actual:', lastTrainingData.output);
+            });
+    })
+    // Load from saved training
+    // .then((trainingData) => {
+    //     return app
+    //         .continueTraining('./trained.txt', trainingData)
+    //         .then(net => {
+    //             const lastTrainingData = trainingData[trainingData.length - 1];
+    //             console.log('result:', net.run(lastTrainingData.input))
+    //             console.log('actual:', lastTrainingData.output);
+    //         });
+    // })
+    .catch(error => console.log(error));
